3298231abb0b5e7e2eeb3bdec66126eea3ce189e
adriano hardtke <adr.hardtke@gmail.com>
2020-09-23 13:24:07 -0300

    modulos

---
+++ introducao.js
@@ -0,0 +1,1 @@
+console.log('Hello World') // to run: node app.js
---
+++ modulo_exemplo.js
@@ -0,0 +1,11 @@
+// const somaFunc = require("./calculadora")
+// console.log(somaFunc(1,2))
+
+// const calculadora = require("./calculadora")
+// console.log(calculadora.soma(3,2))
+
+// const { soma } = require("./calculadora")
+// console.log(soma(3,2))
+
+const { soma: somaFunc} = require("./calculadora")
+console.log(somaFunc(3,3))
