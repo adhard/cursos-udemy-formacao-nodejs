5998612b3dd976e157d5afab2ef1a183e6da67d5
adriano hardtke <adr.hardtke@gmail.com>
2020-09-24 12:21:25 -0300

    parametros

--- index.js
+++ index.js
@@ -6,6 +6,33 @@ server.get('/', (req, res) => {
     return res.send("Olá mundo !!")
 })
 
+server.get('/ola/:nome', (req, res) => {
+    // req.params.nome -> URL/adriano
+    const nome = req.params.nome
+    
+    return res.send("Olá "+nome)
+})
+
+server.get('/oi/:nome?', (req, res) => { //nome é um parametro opcional
+    const nome = req.params.nome
+
+    if(nome) return res.send("Oi "+nome)
+    
+    return res.send("Oi para ninguém")
+})
+
+
+/**
+ * 
+ * Query Params
+ * 
+ */
+
+server.get('/canal/youtube', (req, res) => { //ex: localhost:4001/canal/youtube?canal="bearcode"
+    const canal = req.query["canal"]
+    return res.send("Acesse meu canal no youtube: "+canal)
+})
+
 server.listen(8001, function(erro){
     if(erro) console.log("Erro")
     else console.log("Servidor Iniciado")
