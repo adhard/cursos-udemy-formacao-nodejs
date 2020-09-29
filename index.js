f88e99d65b83d4714c17ab26901039ba3fa994df
adriano hardtke <adr.hardtke@gmail.com>
2020-09-29 10:49:43 -0300

    condicionais e for

--- index.js
+++ index.js
@@ -4,9 +4,29 @@ const app = express()
 // dizendo para o express usar o EJS como view engine
 app.set('view engine', 'ejs')
 
+app.use(express.static('public')) //permite ao nodejs utilizar arquivos estaticos (css, imgs, arquivos html...), "public" é o nome da pasta
+
 app.get('/', (req, res) => {
+
+    const produtos = [
+        {
+            nome: 'notebook',
+            preco: 2000.00
+        },
+        {
+            nome: 'mouse',
+            preco: 150.00
+        },
+        {
+            nome: 'monitor',
+            preco: 1000.00
+        }
+    ]
+
     res.render("index", {
-        nome: "Adriano Hardtke"
+        nome: "Adriano Hardtke",
+        exibirMsg: false,
+        produtos
     }) // index é nome do arquivo .ejs dentro de views
 })
 
